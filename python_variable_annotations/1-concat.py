3a26479962e62bcbc234bdbc756dac3f23c24c04
Maly Medina <jhamilamedina>
2024-02-13 13:02:53 -0800

    Fix: Concat la suma de 2 string

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Función concatenar"""
+
+from typing import Tuple
+
+
+def concat(str1: str, str2: str) -> str:
+
+    """
+    Define la función y concatena 2 números string
+    y retorna el resultado
+
+    Parameters:
+    str1 (str): El primer string
+    str2 (str): El segundo string
+
+    Return:
+    str: string concatenado
+    """
+
+    concatenated_string = str1 + str2
+    return concatenated_string
