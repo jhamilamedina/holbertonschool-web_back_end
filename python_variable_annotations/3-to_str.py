9fc0fd750134f196270ad9b6765ee48d74bff0a6
Maly Medina <jhamilamedina>
2024-02-15 12:39:57 -0800

    Fix: Representacion de un string float

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Devuelve la representacion de
+un string float
+"""
+
+from typing import Tuple
+
+
+def to_str(n: float) -> str:
+    """
+    Convierte un flotante a su
+    representación de cadena.
+
+    parameters:
+    n (float): El nro flotante que se convertirá
+
+    Return:
+    str: La representacion de string float
+    """
+    return str(n)
