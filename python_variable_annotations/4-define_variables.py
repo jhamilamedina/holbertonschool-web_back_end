58a730451e1ae2533c232943afbe6d02c43db3ba
Maly Medina <jhamilamedina>
2024-02-16 21:04:48 -0800

    Fix: Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Definir variables
+"""
+
+from typing import TypedDict
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
