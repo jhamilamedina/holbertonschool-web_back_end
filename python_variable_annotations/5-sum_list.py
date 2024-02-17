d8780f23e8d156a9a3a52e59334b9f74f9f7f644
Maly Medina <jhamilamedina>
2024-02-16 21:37:18 -0800

    Fix: Lista de flotantes

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -7,6 +7,16 @@ from typing import List
 
 
 def sum_list(input_list: List[float]) -> float:
+    """
+    Define una funcion y suma los elementos
+    de la lista de números flotantes
+
+    Args:
+        input_list (List[float]): Lista de nros flotantes
+
+    Return:
+        La suma de los elementos de la lista
+    """
     total: float = 0.0
 
     for num in input_list:
