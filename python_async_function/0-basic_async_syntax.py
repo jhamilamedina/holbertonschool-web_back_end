1b89708948ebed79c7627d3df3cad50cbee76e41
Maly <jhamilamedina98xd@gmail.com>
2024-02-22 01:02:16 +0000

    Fix: Funcion asyncrona

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,21 +7,10 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int = 10) -> float:
     # Generamos un retraso aleatorio entre 0 y max_delay segundos
-    delay = random.uniform(0, max_delay)
+    delay: float = random.uniform(0, max_delay)
     # Esperamos el retraso usando asyncio.sleep()
     await asyncio.sleep(delay)
     # Devolvemos el retraso
     return delay
-
-
-async def main():
-    # Llamamos wait_ramdom con un valor espec√fico
-    delay = await wait_random(5)
-    print(f" Esperamos {delay} segundos")
-
-
-# Ejecutamos el bucle de asyncio para
-# la rutina principal
-asyncio.run(main())
