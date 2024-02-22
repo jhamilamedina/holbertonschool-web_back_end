e0f99cb681603106e497759fe51bb1008a5dacd9
Maly <jhamilamedina98xd@gmail.com>
2024-02-22 01:11:01 +0000

    Fix: Funcion asyncrona

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,8 +7,8 @@ import asyncio
 import random
 
 
-# Definimos una coroutina asincrona
 async def wait_random(max_delay: int = 10) -> float:
+    """Definimos una coroutina wait_random"""
     # Generamos un retraso aleatorio entre 0 y max_delay segundos
     delay: float = random.uniform(0, max_delay)
     # Esperamos el retraso usando asyncio.sleep()
