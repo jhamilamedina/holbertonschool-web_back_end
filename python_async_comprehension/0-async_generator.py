37d51dd2df6ac3cf98c01e6d5079e811ca9309a9
Maly <jhamilamedina98xd@gmail.com>
2024-02-24 04:32:23 +0000

    Fix: Una coroutina repetida 10 veces

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -13,13 +13,11 @@ async def async_generator():
     Define una coroutina asincrona que se repita
     10 veces
     """
-    # La rutina se repetirá10 veces
-    for _ in range(10):
+    # La rutina se repetir 10 veces
+    for _ in range(0, 10):
         # Esperamos 1 seg de forma asincrona
         await asyncio.sleep(1)
-    # Generamos un nro aleatorio del 0 al 10
-    random_number = random.randint(0, 10)
-    yield random_number
+        yield random.uniform(0, 10)
 
 if __name__ == "__main__":
-    asyncio.run(async_generator)
+    async_generator()
