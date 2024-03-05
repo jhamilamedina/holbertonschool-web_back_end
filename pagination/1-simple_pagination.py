9931f91e52a1cbd806e1a70774866630c876989e
Maly <jhamilamedina98xd@gmail.com>
2024-03-05 23:28:00 +0000

    Fix: Simple Pagination importar CSV

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Si los argumentos estan fuera del rango devolver una
+lista vacia
+"""
+
+
+import csv
+import math
+from typing import List
+
+
+# Importamos index_range del modulo 0-simple_helper_function.py
+index_range = __import__('0-simple_helper_function').index_range
