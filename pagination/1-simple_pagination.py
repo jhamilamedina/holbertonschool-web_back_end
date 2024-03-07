acdfc500a8d14562eb93fd8fd51c696b39218aac
Maly <jhamilamedina98xd@gmail.com>
2024-03-07 01:09:43 +0000

    Fix: Simple Pagination

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -35,7 +35,7 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert isinstance(page, int) and page > 0, "La pá > 0"
         assert isinstance(page_size, int) and page_size > 0, "El size > 0"""
-        start_index, end_index = index_range(page, page_size)
+        start_index, end_index = self.index_range(page, page_size)
 
         if start_index >= len(self.dataset()) or end_index <= 0:
             return []
