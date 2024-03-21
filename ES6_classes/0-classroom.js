fdd922617da6393a9199813e794f49db7ffa0af5
Maly <jhamilamedina98xd@gmail.com>
2024-03-20 20:36:31 -0500

    Fix: Crear una clase ClassRom

--- ES6_classes/0-classroom.js
+++ ES6_classes/0-classroom.js
@@ -1,7 +1,11 @@
-// classRoom
-
+// Definimos una clase llamada ClassRoom
+// Exportamos la clase para que esté disponible fuera de el archivo
 export default class ClassRoom {
+    // Constructor: Llama automaticamente a la clase
+    // Definimos el atributo maxStudentsSize
     constructor(maxStudentsSize) {
-      this._maxStudentsSize = maxStudentsSize;
+        // this: Hace referencia al objeto actual que se crea
+        // Asignamos un valor a la propiedad _maxStudentsSize
+        this._maxStudentsSize = maxStudentsSize;
     }
-  }
+}
