2d63e2837a5960fe3836c4813db921ba5323e003
Maly <jhamilamedina98xd@gmail.com>
2024-03-23 17:02:23 -0500

    Fix: Clase Currency(moneda)

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,32 @@
+// Se crea una clase Currency
+export default class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter para el atributo 'codigo'
+  get code() {
+    return this._code;
+  }
+
+  // Setter para el atributo 'codigo'
+  set code(newCode) {
+    this._code = newCode;
+  }
+
+  // Getter para el atributo 'nombre'
+  get name() {
+    return this._name;
+  }
+
+  // Setter para el atributo 'nombre'
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // Método que muestra los atributos en formato específico
+  displayFullCurrency() {
+    return `(${this._name}) ${this._code}`;
+  }
+}
