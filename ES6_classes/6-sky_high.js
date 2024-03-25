59a78810b2992b3e730c2be0151838821dbced49
Maly <jhamilamedina98xd@gmail.com>
2024-03-25 18:09:50 -0500

    feat: Crear la clase SkyHighBuilding que hereda de Building

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -3,21 +3,16 @@
 import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
-  // Atributos del constructor:
   constructor(sqft = 0, floors = 0) {
-    // Llame al constructor de la clase principal usando super
     super(sqft);
-    // floors (Number)
     this._floors = floors;
   }
 
-  // Implemente un getter para cada atributo.
   get floors() {
     return this._floors;
   }
 
-  // Anula el método llamado evacuationWarningMessage
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
