38355b9afc8d4a683eb319c83ef23bae52152f2a
Maly <jhamilamedina98xd@gmail.com>
2024-03-26 22:38:45 -0500

    feat: Implementar la clase Car

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,15 @@
+// Se implementa la clase Car
+
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color
+  }
+
+  // Método cloneCar
+  cloneCar() {
+    // Devuelve un nuevo objeto de la clase con los mismos atributos.
+    return new Car(this._brand, this._motor, this._color)
+  }
+}
