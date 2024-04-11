dc4013cf376afb359fe4719b14e0143b5d495693
Maly <jhamilamedina98xd@gmail.com>
2024-04-10 21:39:11 -0500

    feat: Obtener la respuesta de la API en consola

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,16 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-  // Agregamos un manejador utilizando el método .then()
-  promise.then(() => {
-    // Si la promesa se resuelve correctamente, mostramos un mensaje en la consola
-    console.log('Got a response from the API');
-    // Retornamos un objeto con los atributos solicitados
-    return { status: 200, body: 'success' };
-  })
-  // Agregamos un manejador utilizando el método .catch()
-    .catch((error) => {
-    // Si la promesa es rechazada, mostramos un mensaje en la consola
-      console.log(error);
-      // Retornamos un objeto Error vacío
-      return new Error();
+  return promise
+    .then(() => {
+      const response = {
+        status: 200,
+        body: 'success',
+      };
+      return response;
+    })
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
     });
 }
