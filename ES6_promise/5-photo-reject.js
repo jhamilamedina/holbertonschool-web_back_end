0b2b1f8da3b3f35bfff521e0b4084041ff341ede
Maly <jhamilamedina98xd@gmail.com>
2024-04-13 16:32:38 -0500

    feat: Rechaza una argumento nombre de archivo(string)

--- ES6_promise/5-photo-reject.js
+++ ES6_promise/5-photo-reject.js
@@ -2,10 +2,10 @@
 
 export default function uploadPhoto(filename) {
   return new Promise((resolve, reject) => {
-    if (typeof filename == 'string') {
-      reject(new Error('${fileName} cannot be processed'));
-    }else{
-      resolve('The photo ${fileName} has been successfully uploaded.');
+    if (typeof filename === 'string') {
+      reject(new Error('$fileName cannot be processed'));
+    } else {
+      resolve('');
     }
   });
 }
