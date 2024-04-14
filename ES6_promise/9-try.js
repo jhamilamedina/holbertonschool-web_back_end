9c328eb08a5acf3039092049481f06407004e847
Maly <jhamilamedina98xd@gmail.com>
2024-04-14 13:00:20 -0500

    feat: Throw error / try catch

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,16 +1,16 @@
 // Escribir la función guardrail y definir el parámetro
 export default function guardrail(mathFunction) {
   // Dentro de la función, crear un array llamado queue
-  let queue = [];
+  const queue = [];
   // Ejecutar la función mathFunction y manejar los resultados y errores
   try {
-    let result = mathFunction(); // Ejecutar la función mathFunction
+    const result = mathFunction(); // Ejecutar la función mathFunction
     queue.push(result); // Agregar el resultado a la cola
   } catch (error) {
     queue.push(error.message); // Agregar el mensaje de error a la cola si ocurre un error
   } finally {
     queue.push('Guardrail was processed'); // Agregar el mensaje "Guardrail was processed" a la cola
   }
-  // Devolver la queue después de que se haya ejecutado la función mathFunction y se haya agregado a la cola
+  // Devolver la queue después de que se haya ejecutado la función mathFunction
   return queue;
 }
