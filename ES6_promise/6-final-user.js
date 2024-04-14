1a20fe4cbbece01e07393adea552ba55acc313d6
Maly <jhamilamedina98xd@gmail.com>
2024-04-14 13:29:56 -0500

    feat:Manejas multiples promesas

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,17 +4,11 @@ import uploadPhoto from './5-photo-reject';
 
 // Definir y exportar la función handleProfileSignup
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Llamar a signUpUser con firstName y lastName
-  const signUpPromise = signUpUser(firstName, lastName);
-
-  // Llamar a uploadPhoto con fileName
-  const uploadPhotoPromise = uploadPhoto(fileName);
-
-  // Esperar a que todas las promesas se resuelvan o se rechacen
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise]).then((results) =>
-    // Mapear los resultados a un array con la estructura requerida
-    results.map((result) => ({
-      status: result.status, // status de la promesa (fulfilled o rejected)
-      value: result.status === 'fulfilled' ? result.value : result.reason, // valor o error devuelto por la promesa
-    })));
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  })));
 }
