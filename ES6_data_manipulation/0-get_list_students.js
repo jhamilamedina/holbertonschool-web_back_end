68c3acbb2c645b5eab85b0bb5faa617d1603dd7d
Maly <jhamilamedina98xd@gmail.com>
2024-04-16 22:46:19 -0500

    feat: Lista de objetos(Devolver un array)

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+// Definimos una funcion que devuelva una matriz de objetos
+export default function getListStudents() {
+  // Creamos una matriz
+  const array = [
+    {id: 1, firsName: 'Guillaume', location: 'San Francisco' },
+    {id: 2, firsName: 'James', location: 'Columbia' },
+    {id: 3, firsName: 'Serena', location: 'San Francisco' }
+  ];
+
+  // Devolvemos el array
+  return array;
+}
