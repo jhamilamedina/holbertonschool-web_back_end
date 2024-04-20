cbf5f91619f4ce93eac90b99795a99e69c0908d3
Maly <jhamilamedina98xd@gmail.com>
2024-04-20 00:27:46 -0500

    feat: Devolver estudiantes con nueva calificacion

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,18 @@
+/** Cree una funcion que devuelva una serie de estudiantes para una 
+ciudad específica con su nueva calificación */
+
+export default function updateStudentGradeByCity(listStudents, city, newGrades) {
+  // Verificar que los parámetros sean válidos
+  if (!Array.isArray(listStudents) || typeof city !== 'string' || !Array.isArray(newGrades)) {
+    throw new Error('Los parámetros no son válidos.');
+  }
+
+  // Filtrar estudiantes por ciudad y actualizar calificaciones
+  return listStudents
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = matchingGrade ? matchingGrade.grade : 'N/A';
+      return { ...student, grade };
+    });
+}
