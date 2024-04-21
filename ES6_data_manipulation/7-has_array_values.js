8c464092a4f61709ee75dec31bcceafd6e85d1bd
Maly <jhamilamedina98xd@gmail.com>
2024-04-21 17:04:00 -0500

    feat: Devuelve un valor booleano si los elem del array existen en el conjunto

---
+++ ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,11 @@
+/** Cree una función que devuelva un valor booleano si todos 
+ * los elementos de la matriz existen dentro del conjunto */
+
+export default function hasValuesFromArray(set, array) {
+  for (const item of array) {
+    if (!set.has(item)) {
+      return false;
+    }
+  }
+  return true;
+}
