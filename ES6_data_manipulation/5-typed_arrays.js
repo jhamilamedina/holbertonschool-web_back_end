e3bd7d120fe4a080ef58a0574f77c300b9a5aa4b
Maly <jhamilamedina98xd@gmail.com>
2024-04-21 16:26:44 -0500

    feat:Devuelve un nuevo ArrayBuffer con un valor Int8 en determinada posición

---
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,27 @@
+/** Crea una función que devuelva un nuevo ArrayBuffer con un
+ * valor Int8 en una posición específica */
+
+export default function createInt8TypedArray(length, position, value) {
+/** Se crea la función createInt8TypedArray que recibe 3 argumentos
+ * Args:
+ *  length: longitud del nuevo ArrayBuffer
+ *  position: la posición en la que se colocará el valor
+ *  value: valor Int8 que se añadirá en la posición determinada
+*/
+  // Verificamos si esta dentro del rango valido del ArrayBuffer y si no
+  // se lanza un error
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length); // Creamos un nuevo ArrayBuffer
+
+  // Creamos una vista Int8Array del ArrayBuffer para poder acceder y
+  // modificar los datos en formato Int8
+  const int8View = new Int8Array(buffer);
+
+  // Asignamos el valor value en la posición position dentro del Int8Array
+  int8View[position] = value;
+
+  return buffer;
+}
