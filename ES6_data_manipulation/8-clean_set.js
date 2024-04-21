39d8b46a93781c807a3866d41defd33585edec6b
Maly <jhamilamedina98xd@gmail.com>
2024-04-21 17:20:29 -0500

    feat: Función que devuelva una cadena de todos los valores dados

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,10 @@
 export default function cleanSet(set, startString) {
-  let result = ''; // Paso 1: Inicializa una cadena vacía
-
-  set.forEach((value) => { // Paso 2: Itera sobre cada valor en el conjunto
-    if (value.startsWith(startString)) { // Paso 3: Verifica si el valor comienza con startString
-      const restOfString = value.slice(startString.length); // Paso 4: Obtiene el resto de la cadena
-      result += (result === '' ? '' : '-') + restOfString; // Agrega el valor a la cadena resultante
-    }
-  });
-
-  return result; // Paso 5: Devuelve la cadena resultante
+  let txt = '';
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  if (startString !== '') {
+    txt = filteredValues
+      .map((value) => value.substring(startString.length))
+      .join('-');
+  }
+  return txt;
 }
